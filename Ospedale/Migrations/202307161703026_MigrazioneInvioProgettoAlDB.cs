218358fcce7adf6f37392aaceb8569355f92ea6a
giuli <giuli@LAPTOP-0NAULSQG>
2023-11-01 15:24:31 +0100

    Aggiungere i file di progetto.

---
+++ Ospedale/Migrations/202307161703026_MigrazioneInvioProgettoAlDB.cs
@@ -0,0 +1,22 @@
+﻿namespace Ospedale.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class MigrazioneInvioProgettoAlDB : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Medico", "IdReparto", c => c.Int(nullable: false));
+            CreateIndex("dbo.Medico", "IdReparto");
+            AddForeignKey("dbo.Medico", "IdReparto", "dbo.Reparto", "IdReparto", cascadeDelete: true);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Medico", "IdReparto", "dbo.Reparto");
+            DropIndex("dbo.Medico", new[] { "IdReparto" });
+            DropColumn("dbo.Medico", "IdReparto");
+        }
+    }
+}
